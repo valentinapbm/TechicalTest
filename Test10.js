2ce4b23ab15eb9c2c3ebb3a809ab5ed69e16a991
Valentina Paredes Baquero <97267260+valentinapbm@users.noreply.github.com>
2023-04-13 16:14:50 -0500

    Update Test10.js

--- Test10.js
+++ Test10.js
@@ -1,4 +1,63 @@
 //TEST 10
+
+/* Pseudocódigo
+Proceso mergeSortAlgorithm (unorderedArray) 
+
+Si la longitud de unorderedArray es menor a 2  modulo 
+
+	Devolver unorderedArray 
+
+Crear variable middleIndex y encontrar el indice del medio 
+
+Crear variable firstHalf y dividir la primera mitad 
+
+Crear variable secondHalf y dividir la segunda mitad
+
+ 
+
+Devolver ordernarArray((mergeSortAlgorithm(firstHalf),mergeSortAlgorithm(secondHalf)) 
+
+ 
+
+Proceso ordenarArray(firstHalf, secondHalf) 
+
+Crear un nuevo array para asignar los valores llamado orderedArray 
+
+Crear variable leftIndex ubicado en 0  
+
+Crear variable rightIndex ubicado en 0 
+
+ 
+
+Mientras el leftIndex sea menor que la longitud de firstHalf y el rightIndex sea menor que la longitud de secondHalf 
+
+	Si firstHalf[leftIndex] < secondHalf[rightIndex] 
+
+		agregar a orderedArray firstHalf[leftIndex] 
+
+Añadir 1 al leftIndex 
+
+	Sino  
+
+		agregar a orderedArray secondHalf[rightIndex] 
+
+		Añadir 1 al rightIndex 
+
+Termina el ciclo 
+
+Si firstHalf[leftIndex] existe o es diferente de undefined 
+
+Añadir el restante de firstHalf al orderedArray 
+
+Sino  
+
+Añadir el restande de secondHalf al orderedArray 
+
+Devolvemos el orderedArray 
+
+
+
+*/
 function mergeSortAlgorithm (unorderedArray){
     if (unorderedArray.length <2){
         return unorderedArray;
